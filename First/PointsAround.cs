6b0de88165760bb56e64a5f060d90c0add6d775a
Neither <nikita.badanin2010@yandex.ru>
2016-10-29 23:45:49 +0300

    Написала класс соседних координат
    Есть поля типа Position для четырех соседних клеток
    Конструктор есть
    Есть метод обновления соседних координат.
    Должен вызываться после каждого изменения координат исходным объектом.

---
+++ First/PointsAround.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace First
+{
+    class PointsAround
+    {
+        private Position Up = new Position();
+        private Position Down = new Position();
+        private Position Left = new Position();
+        private Position Right = new Position();
+
+        public PointsAround(Position point)
+        {
+            AroundPointsUpdate(point);
+        }
+        /// <summary>
+        /// Метод обновления соседних координат
+        /// </summary>
+        /// <param name="point">Координаты исходного объекта</param>
+        public void AroundPointsUpdate(Position point)
+        {
+            //ToDo Выяснить, можно ли выполнить операцию присвоения группе объектов, записанную через запятую
+            Up = point;
+            Down = point;
+            Left = point;
+            Right = point;
+            Up.x--;
+            Down.x++;
+            Left.y--;
+            Right.y++;
+        }
+    }
+}
